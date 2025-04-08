938a888d2223aae04f272b9da02a643eb5bbddf0
Amar Rama <ramaamar@gmail.com>
2025-04-08 20:15:28 +0200

    Add feedback feature with prop-types and card component
    - Introduced FeedbackData for managing feedback entries.
    - Created Card component for displaying individual feedback.
    - Added Image and Title components for better structure.
    - Integrated prop-types for type checking in Card component.
    - Updated package.json and package-lock.json to include prop-types and react-icons.

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { useState } from 'react'
 import './App.css'
 import SideBar from './component/SideBar.jsx'
@@ -30,6 +31,48 @@ function App() {
       </div>
     </div>
   );
+=======
+import React, { useState } from 'react'
+import './App.css'
+import FeedbackData from './data/feedbackData'
+import Card from './components/Card.jsx'
+console.log(FeedbackData)
+
+
+
+function App() {
+  const [feedbackData, setFeedbackData] = useState(FeedbackData)
+  const deleteFeedBack = (id) => {
+    if (window.confirm(`Are you sure you want to delete feedback with id: ${id}?`)) {
+      console.log(`Delete feedback with id: ${id}`);
+      setFeedbackData((prevFeedback) => prevFeedback.filter((item) => item.id !== id));
+    } else {
+      console.log(`Deletion canceled for feedback with id: ${id}`);
+    }
+  };
+  console.log(feedbackData)
+  return (
+    < >
+      <h1>Feedback List</h1>
+
+      <div style={{ gap: '50px', justifyContent: 'Center', alignContent: 'cetner', border: '1px solid #ccc', borderRadius: '8px', padding: '16px' }}>
+        {feedbackData.map((item) => (
+          console.log(item),
+          <Card
+            key={item.id}
+            title={item.text}
+            rating={item.rating}
+            src={item.src}
+            deleteFeedBack={() => 
+              deleteFeedBack(item.id)
+              // console.log(`Delete feedback with id: ${item.id}`)
+          }
+          />
+        ))}
+      </div>
+    </>
+  )
+>>>>>>> 5bdf3ae (Add feedback feature with prop-types and card component)
 }
 
-export default App
+export default App;
