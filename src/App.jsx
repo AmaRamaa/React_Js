cffef7cea4c163520ad17c2b460ab5422b87dd8b
AmaRamaa <ramaamar310@gmail.com>
2025-04-11 13:37:58 +0200

    refactor App component structure and add Tailwind CSS configuration

--- src/App.jsx
+++ src/App.jsx
@@ -58,7 +58,7 @@ function App() {
             } else {
               return (
                 <>
-                  <ReturnPage />
+                  <ReturnPage/>
                 </>
               );
             }
@@ -66,6 +66,11 @@ function App() {
         </div>
       )}
     </>
+  );
+  return (
+    <div className="bg-blue-500 text-white p-4">
+        <h1 className="text-2xl font-bold">Hello, Tailwind CSS!</h1>
+    </div>
   );
 }
 
